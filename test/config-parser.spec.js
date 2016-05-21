9f455fd4ddb0a8037b44416dd01461b027517432
Tom Duncalf <tom@tomduncalf.com>
2016-05-21 13:48:05 +0100

    Move tests into a separate folder for ease of building

--- test/config-parser.spec.js
+++ test/config-parser.spec.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import fs from 'fs'
 
-import parse from './config-parser'
+import parse from '../src/config-parser'
 
 const configIni = fs.readFileSync('../fixtures/Devastor.cfg', 'utf-8')
 const configObj = JSON.parse(fs.readFileSync('../fixtures/Devastor.json', 'utf-8'))
