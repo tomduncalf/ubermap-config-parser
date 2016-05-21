32eee53a86b6130de2007f6b395c8a05ee241571
Tom Duncalf <tom@tomduncalf.com>
2016-05-21 13:55:19 +0100

    Add casting support

--- src/config-parser.js
+++ src/config-parser.js
@@ -1,6 +1,6 @@
 import i from 'icepick'
 
-export default function parse(config) {
+export default function parse(config, cast = true) {
     let result = {}
     const lines = config.split("\n").filter(line => line).map(line => line.trim())
 
@@ -11,8 +11,19 @@ export default function parse(config) {
         } else if (line.substr(0, 1) === '[') {
             key = [line.slice(1, -1)]
         } else {
-            const split = line.split('=').map(s => s.trim())
-            result = i.assocIn(result, [...key, split[0]], split[1])
+            let [localKey, value] = line.split('=').map(s => s.trim())
+
+            if (cast) {
+                if (value === 'True') {
+                    value = true
+                } else if (value === 'False') {
+                    value = false
+                } else if (value.indexOf(',') > -1) {
+                    value = value.split(',').map(s => s.trim())
+                }
+            }
+
+            result = i.assocIn(result, [...key, localKey], value)
         }
     })
 
--- test/config-parser.spec.js
+++ test/config-parser.spec.js
@@ -4,8 +4,13 @@ import fs from 'fs'
 import parse from '../src/config-parser'
 
 const configIni = fs.readFileSync('../fixtures/Devastor.cfg', 'utf-8')
-const configObj = JSON.parse(fs.readFileSync('../fixtures/Devastor.json', 'utf-8'))
 
-test('parser', t => {
+test('parser without casting', t => {
+    const configObj = JSON.parse(fs.readFileSync('../fixtures/Devastor.json', 'utf-8'))
+    t.deepEqual(parse(configIni, false), configObj)
+})
+
+test('parser with casting', t => {
+    const configObj = JSON.parse(fs.readFileSync('../fixtures/Devastor-cast.json', 'utf-8'))
     t.deepEqual(parse(configIni), configObj)
 })
